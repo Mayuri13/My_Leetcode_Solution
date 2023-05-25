e9bcb969e7f1c86d27b5c18c9ef70cbf434372e8
Mayuri13 <78290636+Mayuri13@users.noreply.github.com>
2023-05-25 20:09:48 +0530

    Time: 8 ms (89.63%), Space: 6.4 MB (98.01%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,25 +1,49 @@
 class Solution {
 public:
-    //Tabulation Solution
+    //Space Optimization
     int minDistance(string w1, string w2) {
         int n = w1.size(), m = w2.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        vector<int>prev(m+1,0), curr(m+1,0);
         for(int j=1; j<=m; j++){
-            dp[0][j] = j;
+            prev[j] = j;
         }
+        
         for(int i=1; i<=n; i++){
-            dp[i][0] = i;
-        }
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=m; j++){
-                if(w1[i-1]==w2[j-1])
-                    dp[i][j] = dp[i-1][j-1];
-                else
-                    dp[i][j] = min(min(1+dp[i-1][j],1+dp[i-1][j-1]),1+dp[i][j-1]);
+            for(int j=0; j<=m; j++){
+                if(j==0)
+                    curr[j] = i;
+                else{
+                    if(w1[i-1]==w2[j-1])
+                        curr[j] = prev[j-1];
+                    else
+                        curr[j] = min(min(1+prev[j],1+prev[j-1]),1+curr[j-1]);
+                }
             }
+            prev = curr;
         }
-        return dp[n][m];
+        return prev[m];
     }
+    
+    //Tabulation Solution
+    // int minDistance(string w1, string w2) {
+    //     int n = w1.size(), m = w2.size();
+    //     vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+    //     for(int j=1; j<=m; j++){
+    //         dp[0][j] = j;
+    //     }
+    //     for(int i=1; i<=n; i++){
+    //         dp[i][0] = i;
+    //     }
+    //     for(int i=1; i<=n; i++){
+    //         for(int j=1; j<=m; j++){
+    //             if(w1[i-1]==w2[j-1])
+    //                 dp[i][j] = dp[i-1][j-1];
+    //             else
+    //                 dp[i][j] = min(min(1+dp[i-1][j],1+dp[i-1][j-1]),1+dp[i][j-1]);
+    //         }
+    //     }
+    //     return dp[n][m];
+    // }
     
     //Memoization Solution
 //     int find(int i, int j, vector<vector<int>>&dp, string w1, string w2){
