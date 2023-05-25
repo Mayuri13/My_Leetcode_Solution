433f9faba67f0469d7555bfac5a58eebc48d1acb
Mayuri13 <78290636+Mayuri13@users.noreply.github.com>
2023-05-25 12:17:44 +0530

    Time: 49 ms (59.53%), Space: 6.3 MB (99.52%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,26 +1,48 @@
 class Solution {
 public:
-    //Tabulation Solution
+    //Space Optimization
     int prime = 1e9+7;
     int numDistinct(string s, string t) {
         int n = s.size(), m = t.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-        dp[0][0] = 1;
+        vector<int>prev(m+1,0);
+        prev[0] = 1;
         for(int i=1; i<=n; i++){
-            for(int j=0; j<=m; j++){
+            for(int j=m; j>=0; j--){
                 if(j==0)
-                    dp[i][j] = 1;
+                    prev[j] = 1;
                 else{
                     if(s[i-1]==t[j-1])
-                        dp[i][j] = (dp[i-1][j-1] + dp[i-1][j])%prime;
+                        prev[j] = (prev[j-1] + prev[j])%prime;
                     
                     else
-                        dp[i][j] = dp[i-1][j];
+                        prev[j] = prev[j];
                 }
             }
         }
-        return dp[n][m];
+        return prev[m];
     }
+    
+    //Tabulation Solution
+//     int prime = 1e9+7;
+//     int numDistinct(string s, string t) {
+//         int n = s.size(), m = t.size();
+//         vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+//         dp[0][0] = 1;
+//         for(int i=1; i<=n; i++){
+//             for(int j=0; j<=m; j++){
+//                 if(j==0)
+//                     dp[i][j] = 1;
+//                 else{
+//                     if(s[i-1]==t[j-1])
+//                         dp[i][j] = (dp[i-1][j-1] + dp[i-1][j])%prime;
+                    
+//                     else
+//                         dp[i][j] = dp[i-1][j];
+//                 }
+//             }
+//         }
+//         return dp[n][m];
+//     }
     
     
     //Memoization Solution
