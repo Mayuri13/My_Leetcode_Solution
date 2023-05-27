0e150f1867ffbf8bba590733c3c789f5ce4f9f03
Mayuri13 <78290636+Mayuri13@users.noreply.github.com>
2023-05-27 17:10:39 +0530

    Time: 293 ms (61.69%), Space: 75.3 MB (80.66%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,23 +1,43 @@
 class Solution {
 public:
-    //Tabulation Solution
+    //Space Optimization
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(5,0)));
+        vector<vector<int>>prev(2,vector<int>(5,0)), curr(2,vector<int>(5,0));
         for(int i=n-1; i>=0; i--){
             for(int j=1; j>=0; j--){
                 for(int k=3; k>=0; k--){
                     if(j){
-                        dp[i][j][k]= max((-prices[i]+dp[i+1][0][k+1]),(0+dp[i+1][1][k]));
+                        curr[j][k]= max((-prices[i]+prev[0][k+1]),(0+prev[1][k]));
                     }
                     else{
-                        dp[i][j][k]= max((prices[i]+dp[i+1][1][k+1]),(0+dp[i+1][0][k]));
+                        curr[j][k]= max((prices[i]+prev[1][k+1]),(0+prev[0][k]));
                     }
                 }
             }
+            prev = curr;
         }
-        return dp[0][1][0];
+        return prev[1][0];
     }
+    
+    //Tabulation Solution
+//     int maxProfit(vector<int>& prices) {
+//         int n = prices.size();
+//         vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(5,0)));
+//         for(int i=n-1; i>=0; i--){
+//             for(int j=1; j>=0; j--){
+//                 for(int k=3; k>=0; k--){
+//                     if(j){
+//                         dp[i][j][k]= max((-prices[i]+dp[i+1][0][k+1]),(0+dp[i+1][1][k]));
+//                     }
+//                     else{
+//                         dp[i][j][k]= max((prices[i]+dp[i+1][1][k+1]),(0+dp[i+1][0][k]));
+//                     }
+//                 }
+//             }
+//         }
+//         return dp[0][1][0];
+//     }
     
     
     //Recursive Solution
