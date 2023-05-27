596bbc4606c36ab9b8b67d6a20328e8390158ea4
Mayuri13 <78290636+Mayuri13@users.noreply.github.com>
2023-05-27 22:35:06 +0530

    Time: 273 ms (10.35%), Space: 94.6 MB (34.18%) - LeetHub

---
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+     //Memoization Solution
+    int profit(int i, int buy, vector<vector<int>>&dp, int fee, vector<int>&prices){
+        if(i>=prices.size())
+            return 0; //as no monwy can be returned.. days exhausted
+        if(dp[i][buy]!=-1)
+            return dp[i][buy];
+        // int p = 0;
+        if(buy){
+            dp[i][buy] = max((-prices[i]+profit(i+1,0,dp,fee,prices)),(0+profit(i+1,1,dp,fee,prices)));
+        }
+        //when we have already bought, so now we can either sale on ith day or not sale
+        else{
+            dp[i][buy] = max((prices[i]-fee+profit(i+1,1,dp,fee,prices)),(0+profit(i+1,0,dp,fee,prices)));
+        }
+        return dp[i][buy];
+    }
+    int maxProfit(vector<int>& prices, int fee) {
+        int n = prices.size();
+        vector<vector<int>>dp(n,vector<int>(2,-1));
+        return profit(0,1,dp,fee,prices);
+    }
+};
