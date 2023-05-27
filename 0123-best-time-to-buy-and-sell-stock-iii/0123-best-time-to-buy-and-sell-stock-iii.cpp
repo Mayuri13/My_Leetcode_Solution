c3ed3952599e3391b88a1a4f3f328a8c3cb37163
Mayuri13 <78290636+Mayuri13@users.noreply.github.com>
2023-05-27 17:08:24 +0530

    Time: 843 ms (13.59%), Space: 230.7 MB (6.18%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    //Tabulation Solution
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(5,0)));
+        for(int i=n-1; i>=0; i--){
+            for(int j=1; j>=0; j--){
+                for(int k=3; k>=0; k--){
+                    if(j){
+                        dp[i][j][k]= max((-prices[i]+dp[i+1][0][k+1]),(0+dp[i+1][1][k]));
+                    }
+                    else{
+                        dp[i][j][k]= max((prices[i]+dp[i+1][1][k+1]),(0+dp[i+1][0][k]));
+                    }
+                }
+            }
+        }
+        return dp[0][1][0];
+    }
+    
+    
+    //Recursive Solution
+    // int profit(int i, int buy, int cnt, vector<int>&prices){
+    //     if(i==prices.size())
+    //         return 0;
+    //     if(cnt==4)
+    //         return 0;
+    //     // if(dp[i][buy]!=-1)
+    //     //     return dp[i][buy];
+    //     int p = 0;
+    //     if(buy){
+    //         p = max((-prices[i]+profit(i+1,0,cnt+1,prices)),(0+profit(i+1,1,cnt,prices)));
+    //     }
+    //     //when we have already bought, so now we can either sale on ith day or not sale
+    //     else{
+    //         p = max((prices[i]+profit(i+1,1,cnt+1,prices)),(0+profit(i+1,0,cnt,prices)));
+    //     }
+    //     return p;
+    // }
+    // int maxProfit(vector<int>& prices) {
+    //     return profit(0,1,0,prices);
+    // }
+};
