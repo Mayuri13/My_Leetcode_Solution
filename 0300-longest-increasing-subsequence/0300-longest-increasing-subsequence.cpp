7f8b66daec60229c8183f4e4e00161f857d572d6
Mayuri13 <78290636+Mayuri13@users.noreply.github.com>
2023-05-30 17:35:32 +0530

    Time: 1713 ms (5.00%), Space: 291 MB (26.13%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,5 +1,22 @@
 class Solution {
 public:
+    // //Tabulation Solution
+    // int lengthOfLIS(vector<int>& nums) {
+    //     int n = nums.size();
+    //     vector<vector<int>>dp(n+1,vector<int>(n,0));
+    //     for(int i=n-1; i>=0; i--){
+    //         for(int j=n-1; j>=0; j--){
+    //             int p=0;
+    //             if(j==0 || (nums[j]<nums[i]))
+    //                 p = 1+dp[i+1][i];
+    //             int np = dp[i+1][j];
+    //             dp[i][j] = max(p,np);
+    //         }
+    //     }
+    //     return dp[0][0];
+    // }
+    
+    //Memoization Solution
     int find(int i, int prev, vector<vector<int>>&dp, vector<int>&nums){
         if(i==nums.size())
             return 0;
@@ -14,7 +31,7 @@ public:
     }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>>dp(n,vector<int>(n+1,-1));
+        vector<vector<int>>dp(n,vector<int>(n,-1));
         return find(0,-1,dp,nums);
     }
 };
