3043e5beb5116720162985402916e29e46709747
Mayuri13 <78290636+Mayuri13@users.noreply.github.com>
2023-06-19 12:37:49 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
@@ -4,46 +4,29 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution {
-  private: 
-  bool detect(int src, vector<int> adj[], int vis[]) {
-      vis[src] = 1; 
-      // store <source node, parent node>
-      queue<pair<int,int>> q; 
-      q.push({src, -1}); 
-      // traverse until queue is not empty
-      while(!q.empty()) {
-          int node = q.front().first; 
-          int parent = q.front().second; 
-          q.pop(); 
-          
-          // go to all adjacent nodes
-          for(auto adjacentNode: adj[node]) {
-              // if adjacent node is unvisited
-              if(!vis[adjacentNode]) {
-                  vis[adjacentNode] = 1; 
-                  q.push({adjacentNode, node}); 
-              }
-              // if adjacent node is visited and is not it's own parent node
-              else if(parent != adjacentNode) {
-                  // yes it is a cycle
-                  return true; 
-              }
-          }
-      }
-      // there's no cycle
-      return false; 
-  }
   public:
-    // Function to detect cycle in an undirected graph.
+  //DFS Traversal
+    bool find(int src,vector<int> adj[],  vector<int>&vis, int parent){
+        vis[src] = 1;
+        for(auto it:adj[src]){
+            if(!vis[it]){
+                if(find(it,adj,vis,src)==true)
+                    return true;
+            }
+            else if(it!=parent)
+                return true;
+        }
+        return false;
+    }
     bool isCycle(int V, vector<int> adj[]) {
-        // initialise them as unvisited 
-        int vis[V] = {0};
-        for(int i = 0;i<V;i++) {
-            if(!vis[i]) {
-                if(detect(i, adj, vis)) return true; 
+        vector<int>vis(V,0);
+        for(int i=0; i<V; i++){
+            if(!vis[i]){
+                if(find(i, adj, vis, -1)==true)
+                    return true;
             }
         }
-        return false; 
+        return false;
     }
 };
 
