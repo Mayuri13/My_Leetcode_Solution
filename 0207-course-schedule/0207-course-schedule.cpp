a435775e4e5fdf2d0f318d984e6b49aa9c059eeb
Mayuri13 <78290636+Mayuri13@users.noreply.github.com>
2023-06-20 23:14:22 +0530

    Time: 23 ms (71.68%), Space: 14 MB (42.27%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool dfs(int src, vector<int>&vis, vector<int>&pathVis, vector<vector<int>>& adj){
+        vis[src]=1;
+        pathVis[src] = 1;
+        for(auto it:adj[src]){
+            if(pathVis[it]==1)
+                return false;
+            else if(!vis[it]){
+                if(dfs(it,vis,pathVis,adj)==false)
+                    return false;
+            }
+        }
+        pathVis[src]=0;
+        return true;
+    }
+    
+    bool canFinish(int n, vector<vector<int>>& p) {
+        vector<int>vis(n,0), pathVis(n,0);
+        vector<vector<int>>adj(n);
+        for(int i=0; i<p.size(); i++){
+            adj[p[i][1]].push_back(p[i][0]);
+        }
+        for(int i=0; i<n; i++){
+            if(!vis[i]){
+                if(dfs(i,vis,pathVis,adj)==false)
+                    return false;
+            }
+        }
+        
+        return true;
+    }
+};
