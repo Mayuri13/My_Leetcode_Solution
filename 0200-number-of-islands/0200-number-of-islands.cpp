9b6cb56ef584db939a12fcab970c061490e84dba
Mayuri13 <78290636+Mayuri13@users.noreply.github.com>
2023-07-21 21:38:41 +0530

    Time: 37 ms (86.00%), Space: 13.3 MB (47.45%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
-    int DX[4] = {-1, 1, 0, 0};
-    int DY[4] = {0, 0, -1, 1};
-    void find(int x, int y, vector<vector<char>>& grid, vector<vector<int>>& vis){
-        
-        vis[x][y]=1;
-        for (int i = 0; i < 4; i++) {
-            int cx = x + DX[i];
-            int cy = y + DY[i];
-            
-            // Check if the neighboring cell is within the grid boundaries and has a greater value
-            if (cx >= 0 && cx < grid.size() && cy >= 0 && cy < grid[0].size() 
-                && grid[cx][cy]=='1' && vis[cx][cy]!=1) {
-                find(cx, cy, grid, vis);
-            }
+    int dx[4] = {-1,1,0,0};
+    int dy[4] = {0,0,-1,1};
+    void dfs(int r, int c, vector<vector<char>>& grid, vector<vector<int>>& vis){
+        vis[r][c] = 1;
+        for(int i=0; i<4; i++){
+                int nr = r+dx[i];
+                int nc = c+dy[i];
+                if(nr < grid.size() && nr>=0 && nc<grid[0].size() && nc>=0 && grid[nr][nc]=='1'
+                   && !vis[nr][nc]){
+                    dfs(nr,nc,grid,vis);
+                }
         }
-        
     }
-    
     int numIslands(vector<vector<char>>& grid) {
-        int n = grid.size(), m = grid[0].size(), ans=0;
-        vector<vector<int>>vis(n,vector<int>(m,-1));
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(vis[i][j]==-1 && grid[i][j]=='1'){
-                    find(i,j,grid,vis);
+        int m = grid.size(), n = grid[0].size(), ans=0;
+        vector<vector<int>>vis(m, vector<int>(n,0));
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(!vis[i][j] && grid[i][j]=='1'){
+                    dfs(i,j,grid,vis);
                     ans++;
                 }
             }
