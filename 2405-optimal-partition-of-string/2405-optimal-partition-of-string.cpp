679430030bad422f581ce490d6d4e5704e990d3c
Mayuri13 <78290636+Mayuri13@users.noreply.github.com>
2023-08-14 11:54:57 +0530

    Time: 173 ms (55.68%), Space: 72 MB (6.77%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int partitionString(string s) {
         set<char>st;
-        int ans1=0, ans2 = 0;
+        int ans1=0;
         for(int i=0; i<s.size(); i++){
             if(st.find(s[i])!=st.end()){
                 st.clear();
@@ -13,16 +13,6 @@ public:
             
         }
         ans1 +=1 ;
-        for(int i=s.size()-1; i>=0; i--){
-            if(st.find(s[i])!=st.end()){
-                st.clear();
-                ans2++;
-            }
-            
-            st.insert(s[i]);
-            
-        }
-        ans2 +=1 ;
-        return min(ans1,ans2);
+        return ans1;
     }
 };
