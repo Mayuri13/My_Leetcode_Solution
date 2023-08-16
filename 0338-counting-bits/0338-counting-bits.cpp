066e2410f218a1a4e60ab09e3516eb5c17f942d3
Mayuri13 <78290636+Mayuri13@users.noreply.github.com>
2023-08-16 12:20:54 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (63.83%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int>ans(n+1,0);
+        //Using DP and right shift concept
+        vector<int>dp(n+1);
+        dp[0] = 0;
         for(int i=1; i<=n; i++){
-            if((i&(i-1))==0){
-                ans[i]=1;
-                continue;
+            if(i%2==0)
+                dp[i] = dp[i/2];
+            else{
+                dp[i] = dp[i/2] + 1;
             }
-            int cnt=0, j=i;
-            while(j>0){
-                j = j&(j-1);
-                cnt++;
-            }
-            ans[i] = cnt;
         }
-        return ans;
+        return dp;
+        //Using normal bit manipulation
+        
+        // vector<int>ans(n+1,0);
+        // for(int i=1; i<=n; i++){
+        //     if((i&(i-1))==0){
+        //         ans[i]=1;
+        //         continue;
+        //     }
+        //     int cnt=0, j=i;
+        //     while(j>0){
+        //         j = j&(j-1);
+        //         cnt++;
+        //     }
+        //     ans[i] = cnt;
+        // }
+        // return ans;
     }
 };
