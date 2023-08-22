753e2e9e7464179b57562cde7223e9e54213728b
Mayuri13 <78290636+Mayuri13@users.noreply.github.com>
2023-08-22 15:40:47 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (11.31%) - LeetHub

--- 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
@@ -1,12 +1,8 @@
 class Solution {
 public:
     string convertToTitle(int n) {
-//         if(n<=26){
-            
-//         }
         string str = "";
         while(n>26){
-            // cout<<n%26<<" ";
             if(n%26==0){
                 str+='Z';
                 n--;
@@ -17,7 +13,6 @@ public:
             }
             n/=26;
         }
-        // cout<<n;
         if(n<=26)
             str += static_cast<char>(64+n);
         reverse(str.begin(),str.end());
