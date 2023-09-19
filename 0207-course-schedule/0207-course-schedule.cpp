f90662deb51ea000fd610a021661f96731d40d05
Mayuri13 <78290636+Mayuri13@users.noreply.github.com>
2023-09-19 21:56:59 +0530

    Time: 22 ms (33.58%), Space: 14.1 MB (40.71%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
-    //Using detecting a cycle alsorithm
-    bool dfs(int src, vector<int>&vis, vector<int>&pathVis, vector<vector<int>>& adj){
-        vis[src]=1;
+    bool hasCycle(vector<vector<int>>& adj, int src, vector<int>&vis, vector<int>&pathVis){
+        vis[src] = 1;
         pathVis[src] = 1;
         for(auto it:adj[src]){
-            if(pathVis[it]==1)
-                return false;
-            else if(!vis[it]){
-                if(dfs(it,vis,pathVis,adj)==false)
-                    return false;
+            if(pathVis[it] == 1) return true;
+            else if(vis[it] == 0){
+                if(hasCycle(adj, it, vis, pathVis))
+                    return true;
             }
         }
-        pathVis[src]=0;
-        return true;
+        pathVis[src] = 0;
+        return false;
     }
-    
-    bool canFinish(int n, vector<vector<int>>& p) {
-        vector<int>vis(n,0), pathVis(n,0);
-        vector<vector<int>>adj(n);
-        for(int i=0; i<p.size(); i++){
-            adj[p[i][1]].push_back(p[i][0]);
+
+    bool canFinish(int n, vector<vector<int>>& courses) {
+        vector<vector<int>> adj(n);
+        for(int i = 0; i<courses.size(); i++){
+            adj[courses[i][1]].push_back(courses[i][0]);
         }
+        vector<int>vis(n,0);
+        vector<int>pathVis(n,0);
         for(int i=0; i<n; i++){
             if(!vis[i]){
-                if(dfs(i,vis,pathVis,adj)==false)
+                if(hasCycle(adj,i,vis,pathVis))
                     return false;
             }
         }
-        
         return true;
     }
 };
