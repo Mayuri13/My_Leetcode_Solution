1e188fc962ea8bc7afc30a80b6888f3ffd622ad0
Mayuri13 <78290636+Mayuri13@users.noreply.github.com>
2024-06-01 20:37:51 +0530

    Time: 4 ms (85.63%), Space: 10.2 MB (41.48%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -12,7 +12,7 @@ public:
         if(head == NULL || head->next == NULL) return NULL;
 
         ListNode* fast = head, *slow = head, *entry = head;
-        while(fast->next != NULL && fast->next->next != NULL){
+        while(fast != NULL && fast->next != NULL){
             slow = slow->next;
             fast = fast->next->next;
             if(slow == fast){
