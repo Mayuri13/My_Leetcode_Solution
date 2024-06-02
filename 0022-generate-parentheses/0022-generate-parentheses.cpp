cc95fe6602979ca2cf50ff8d603e96d4d46f8b63
Mayuri13 <78290636+Mayuri13@users.noreply.github.com>
2024-06-02 13:49:00 +0530

    Time: 4 ms (48.77%), Space: 15.6 MB (33.99%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     void find(int n, int open, int close, string tmp, vector<string>&ans){
+        //If tmp is of 2n size 
         if(tmp.size() == 2*n){
             ans.push_back(tmp);
             return;
         }
 
+        //First inserting the open bracket
         if(open < n){
             tmp += '(';
             find(n, open+1, close, tmp, ans);
             tmp.pop_back();
         }
 
+        //Inserting the closing brackets only when open > close, means opens are inserted first & then closing
         if(close < open){
             tmp += ')';
             find(n, open, close+1, tmp, ans);
