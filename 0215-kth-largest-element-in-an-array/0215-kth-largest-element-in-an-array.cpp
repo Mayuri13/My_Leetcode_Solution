a77cf49f7df9dd33c32c27679b5fbfe4621eaa0e
Mayuri13 <78290636+Mayuri13@users.noreply.github.com>
2024-06-04 14:05:36 +0530

    Time: 75 ms (91.29%), Space: 61.1 MB (63.26%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,8 +1,12 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
+
+        //Min heap to store k elements with max values in increasing order, so that top element is the kth largest
         priority_queue<int, vector<int>, greater<int>>pq;
+
         for(int i=0; i<nums.size(); i++){
+            //If top element is less than nums[i], then pop bcz we need max valued elements in pq
             if(!pq.empty() && pq.size()==k && pq.top()<nums[i]){
                 pq.pop();
                 pq.push(nums[i]);
